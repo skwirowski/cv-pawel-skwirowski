77c7062e5728e04d1af0a1fc83467ccb2a1c4784
Paweł Skwirowski <pskwirowski@gmail.com>
2023-03-28 18:32:17 +0200

    Update Personal Info & add images

--- scripts/personalInfo.js
+++ scripts/personalInfo.js
@@ -1,31 +1,31 @@
 (function () {
   const personalInfo = [
     {
-      imageSrc: "",
+      imageSrc: "styles/images/locationpin.svg",
       imageAlt: "person location",
       href: "https://goo.gl/maps/W4BjwGjK9HJjpHej7",
       content: "Szczecin",
     },
     {
-      imageSrc: "",
+      imageSrc: "styles/images/email.svg",
       imageAlt: "person email address",
       href: "mailto:pskwirowski@gmail.com",
       content: "pskwirowski@gmail.com",
     },
     {
-      imageSrc: "",
+      imageSrc: "styles/images/mobile.svg",
       imageAlt: "person cellphone number",
       href: "tel:+48601647108",
       content: "+48 601 647 108",
     },
     {
-      imageSrc: "",
+      imageSrc: "styles/images/github.svg",
       imageAlt: "person github profile",
       href: "https://github.com/skwirowski",
       content: "github.com/skwirowski",
     },
     {
-      imageSrc: "",
+      imageSrc: "styles/images/linkedin.svg",
       imageAlt: "person linkedin profile",
       href: "https://www.linkedin.com/in/pawel-skwirowski/",
       content: "/in/pawel-skwirowski",
@@ -45,7 +45,7 @@
     <div class="personal-info">
       <div class="personal-info__item">
         <div class="personal-info__item__image__wrapper">
-          <!-- <img class="personal-info__location__image" src=${data.imageSrc} alt=${data.imageAlt} /> -->
+          <img class="personal-info__item__image" src=${data.imageSrc} alt=${data.imageAlt} />
         </div>
         <a class="personal-info__item__content" href=${data.href}>${data.content}</a>
       </div>
